08d618f62c47e7374d995acf52b330bf68335afd
Shardul Mangade <shardul.mangade@gmail.com>
2011-09-09 16:57:36 +0530

    Directory and File working

--- server.py
+++ server.py
@@ -3,7 +3,7 @@
 # Client side command to communicate to this server
 # curl -d @ssh.py "http://<IP>:<port>/put_ssh_key/"
 
-import string,os
+import string,os, errno
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from threading import Thread
 from SocketServer import ThreadingMixIn
@@ -27,7 +27,7 @@ class POST_check:
         (tmp, ignore, temp) = temp.partition('/')
         (operation, ignore, temp) = temp.partition('/')
 
-        print "handle_roa_request" + operation
+        print "handle_post_request" + operation
         
         if operation == 'file_data':
             self.file_data()
@@ -35,15 +35,35 @@ class POST_check:
         if operation == 'file':
             self.file()
 
+        if operation == 'dir':
+            self.dir()
+        
+    def dir(self):
+        len = int(self.handler.headers.get('Content-Length'))
+        dir_name=self.handler.rfile.read(len)
+        try:
+            os.mkdir('/home/shardul/' + dir_name)
+        except OSError, e:
+            if e.errno != errno.EEXIST:
+                raise
+        self.handler.server.table.change_entry_field(0,1,dir_name)
+        print self.handler.server.table.entry_list[0]
+        self.sendResponse(200)
+        return 0
+    
+            
     def file(self):
         len = int(self.handler.headers.get('Content-Length'))
         file_name=self.handler.rfile.read(len)
-        f=open('/home/shardul/' + file_name, 'w')
+        dir_name=self.handler.server.table.get_value(0,1)
+        print '/home/shardul/' + dir_name + '/' + file_name
+        f=open('/home/shardul/' + dir_name + '/' + file_name, 'w')
         print file_name
-        self.handler.server.cur_file=file_name
-        self.handler.server.table.add_entry(0,0,file_name,0)
-        
+        f.close()
+        self.handler.server.table.add_entry(0,'',file_name,0)
+        self.handler.server.table.change_entry_field(0, 2, file_name)
         self.sendResponse(200)
+        
         return 0
        
         
@@ -51,12 +71,10 @@ class POST_check:
 	ip  = self.handler.client_address[0]
         len = int(self.handler.headers.get('Content-Length'))
         file_data=self.handler.rfile.read(len)
-        print "Data " + file_data
-        print "FILE NAME " + self.handler.server.cur_file
-        file_name=self.handler.server.table.get_file_name(0)
+        dir_name=self.handler.server.table.get_value(0,1)
+        file_name=self.handler.server.table.get_value(0,2)
         print file_name
-        f = open('/home/shardul/'+self.handler.server.cur_file,'a+')
-        f=open('/home/shardul/'+ file_name,'a+')
+        f=open('/home/shardul/'+ dir_name + '/' + file_name,'a+')
         f.write(file_data + '\n')
         f.close()
         self.sendResponse(200)
@@ -87,13 +105,22 @@ class metaTable():
     def add_entry(self, index, cur_dir, cur_file, size):
         self.entry_list.append([index, cur_dir, cur_file, size])
 
-    def get_file_name(self, index):
+    def change_entry_field(self, index, value_key, value):
         list_len= len(self.entry_list)
         print list_len
         for i in range(0,list_len):
             if self.entry_list[i][0]==index:
                 print 'SUCCESS'
-                return self.entry_list[i][2]
+                self.entry_list[i][value_key]=value
+                return 0
+        
+    def get_value(self, index, value_key):
+        list_len= len(self.entry_list)
+        print list_len
+        for i in range(0,list_len):
+            if self.entry_list[i][0]==index:
+                print 'SUCCESS'
+                return self.entry_list[i][value_key]
 #TODO : Handle case of failure            
         return ''
     
